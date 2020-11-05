0a9d29151ba3637e99362900c0e6ef1696dbd7f4
gemcfadyen <gemcfadyen@hotmail.co.uk>
2020-11-05 08:06:50 +0000

    Small refactorings

--- src/main/java/com/spacetime/journeys/controller/validation/PersonalGalacticIdentifierValidator.java
+++ src/main/java/com/spacetime/journeys/controller/validation/PersonalGalacticIdentifierValidator.java
@@ -6,6 +6,7 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class PersonalGalacticIdentifierValidator implements ConstraintValidator<ValidPersonalGalacticIdentifier, String> {
+
     @Override
     public boolean isValid(String identifierToValidate, ConstraintValidatorContext constraintValidatorContext) {
         String regex = "[a-zA-Z]([\\da-zA-Z]{4,9})";
